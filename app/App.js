b52f196eab8b8472ff23a54402035eb14cca3faa
Mora Isaza <jose.luis.mora.isaza@accenture.com>
2019-12-09 17:19:01 -0300

    home y ruteo

--- app/App.js
+++ app/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import AppNavigator from "./HomeNav"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Holaaaaaa</Text>
-    </View>
-  );
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component{
+render (){
+  return <AppContainer />
 }
 
+}
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+      },
+      text: {
+        color: "red",
+      }
 });
