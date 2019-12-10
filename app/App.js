07a59d02ea2f150963bcb937c31f94a0fc982239
Barroetavena <facundo.barroetavena@accenture.com>
2019-12-10 14:41:47 -0300

    home carousel

--- app/App.js
+++ app/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CityCarousel from './CityCarousel'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Holaaaaaa</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+  render = () => {
+    return (
+        <CityCarousel/>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%',
   },
 });
