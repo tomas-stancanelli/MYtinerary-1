2f6111ae2039ed2c794627bddb6d6a10899b4cbe
tomas-stancanelli <tomas.stancanelli@accenture.com>
2019-12-10 14:30:43 -0300

    .

--- app/App.js
+++ app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import AppNavigator from "./HomeNav"
+import AppNavigator from "./Components/HomeNav"
 
 
 const AppContainer = createAppContainer(AppNavigator);
---
+++ app/Components/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, Button, View, Image } from 'react-native'
+
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Puto el que lee',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+          <View style={styles.container}>
+          <Image style={styles.imgLogo} source={require('../assets/img/MYtineraryLogo2.png')}/>
+          <TouchableOpacity onPress={() => navigate('Cities')}>
+          <Image source={require('../assets/img/circled-right-2.png')}/>
+            </TouchableOpacity>
+          </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        width:"100%",
+        backgroundColor: '#fff',
+        flex: 1,
+        alignItems: 'center',
+      },
+    imgLogo: {
+       marginTop: 80
+      }});
+
+export default HomeScreen;
