50b5191609a41098215ccbef6072ed435803a8c6
Barroetavena <facundo.barroetavena@accenture.com>
2019-12-13 09:45:17 -0300

    rout itineraryScreen

--- app/Components/HomeNav.js
+++ app/Components/HomeNav.js
@@ -3,28 +3,30 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './HomeScreen';
 import CitiesScreen from './CitiesScreen';
-import { Image, View } from 'react-native';
+import { Image, View, StatusBar } from 'react-native';
 import Hamburguer from "./Hamburguer";
 import Profile from "./Profile";
 import LoginScreen from './LoginScreen';
 import CreateAccountScreen from './CreateAccountScreen';
-
-//<Image source={require('./assets/2388994522.png')} style={{width:40, height:40}}/>
+import ItineraryScreen from './ItineraryScreen';
+import CityCarousel from './CityCarousel';
 
 const AppNavigator = createStackNavigator({
     Home: { screen: HomeScreen },
     Cities: { screen: CitiesScreen },
     Login: { screen: LoginScreen },
     Profile: { screen: Profile },
-    CreateAccount:{ screen: CreateAccountScreen }, 
+    CreateAccount:{ screen: CreateAccountScreen },
+    Itinerary: {screen: ItineraryScreen},
+    CityCarousel: {screen: CityCarousel}
  }
 ,  {
     defaultNavigationOptions : ({navigation}) => ({
     header:     (
-            <View style={{paddingBottom: 0, height: '12%', flexDirection:'row' , justifyContent: 'space-between', alignItems: 'flex-end', top: 10, backgroundColor: '#ef3b36'}}>
+        <View style={{marginTop: StatusBar.currentHeight, height: StatusBar.currentHeight * 1.3, flexDirection:'row' , justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#ef3b36'}}>
             <Profile navigation={navigation} />        
             <Hamburguer navigation={navigation} />
-    </View> )
+        </View> )
     })
  } 
 
